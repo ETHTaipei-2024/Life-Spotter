2e32183e60c8c05f0d6e60c9a230f69c370b13f2
ben0024 <ben@sudoresearch.com>
2024-03-23 13:31:10 -0400

    feat: add wasmSrc and workerSc for imageA11r

--- thirdweb-app/src/c2pa/ImageAuthenticator.ts
+++ thirdweb-app/src/c2pa/ImageAuthenticator.ts
@@ -1,5 +1,26 @@
-import { createC2pa, C2paConfig } from "c2pa";
+import { createC2pa, C2paConfig, C2pa, C2paReadResult } from "c2pa";
 
+import { wasmSrc, workerSrc } from "./constants";
 class ImageAuthenticator {
-    
+    private c2pa: C2pa | null = null;
+    constructor() {
+        this.initC2pa();
+    }
+    async initC2pa(): Promise<void> {
+        const config: C2paConfig = {
+            wasmSrc,
+            workerSrc,
+            fetchRemoteManifests: true,
+        };
+        this.c2pa = await createC2pa(config);
+    }
+
+    async readMetadata(image: File): Promise<C2paReadResult | null> {
+        if (!this.c2pa) {
+            return null;
+        }
+        return await this.c2pa?.read(image);
+    }
 }
+
+export default ImageAuthenticator;
---
+++ thirdweb-app/src/c2pa/constants.ts
@@ -0,0 +1,3 @@
+const version = "latest";
+export const wasmSrc = `https://cdn.jsdelivr.net/npm/c2pa@${version}/dist/assets/wasm/toolkit_bg.wasm`;
+export const workerSrc = `https://cdn.jsdelivr.net/npm/c2pa@${version}/dist/c2pa.worker.min.js`;
